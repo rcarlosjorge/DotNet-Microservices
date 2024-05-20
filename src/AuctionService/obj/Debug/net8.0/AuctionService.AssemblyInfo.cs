20333cbfefa21d39976c80fb0c86c1cc086a3b1d
rcarlosjorge <carlosepjorge@gmail.com>
2024-05-20 16:59:03 -0400

    BiddingService added

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fb5015123b4e96323206c8a9cc01d5a8b8c781")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04664d236993f223460bc4b7097fd367f250cf30")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
