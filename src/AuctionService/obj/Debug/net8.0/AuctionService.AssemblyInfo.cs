cf4817a0b233702ce4a40c73222e979f0796be62
rcarlosjorge <carlosepjorge@gmail.com>
2024-05-22 18:11:42 -0400

    Bugfix notification dockerfile

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6022302cbb9789a1221fe7c4dd11c24da53fa431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0933483196085e4c142a9bcf50bf05371ffb36b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
